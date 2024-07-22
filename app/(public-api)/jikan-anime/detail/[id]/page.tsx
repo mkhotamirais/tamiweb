949dde6da12ce2b652ab10f9cbc396956bc22d21
Mkhotamirais <mkhotamirais@gmail.com>
2024-07-22 21:05:31 +0700

    jikan: 2

--- app/(public-api)/jikan-anime/detail/[id]/page.tsx
+++ app/(public-api)/jikan-anime/detail/[id]/page.tsx
@@ -13,76 +13,76 @@ export default async function DetailAnimeId({ params: { id } }: { params: { id: 
       <div className="grid grid-cols-1 text-sm sm:grid-cols-4 gap-x-0 gap-y-8 sm:gap-x-3">
         <div className="col-span-1 flex flex-col gap-4">
           <Image
-            src={anime.images.jpg.image_url}
+            src={anime?.images?.jpg?.image_url}
             width={300}
             height={300}
             alt="..."
             priority
             className="w-full rounded-lg overflow-hidden"
           />
-          <TrailerYoutube data={anime.trailer} />
+          <TrailerYoutube data={anime?.trailer} />
         </div>
         <div className="col-span-3 leading-relaxed flex flex-col gap-2">
           <div>
             <b>Title: </b>
-            {anime.title}
+            {anime?.title}
           </div>
           <div>
             <b>Synopsis: </b>
-            {anime.synopsis}
+            {anime?.synopsis}
           </div>
           <div>
             <b>Genres: </b>
             <div className="inline-flex gap-2 flex-wrap">
-              {anime.genres?.map((item) => (
-                <a href={item.url} key={item.mal_id} className="underline">
-                  {item.name}
+              {anime?.genres?.map((item) => (
+                <a href={item?.url} key={item?.mal_id} className="underline">
+                  {item?.name}
                 </a>
               ))}
             </div>
           </div>
           <div>
             <b>Year: </b>
-            {anime.year}
+            {anime?.year}
           </div>
           <div>
             <b>Episodes: </b>
-            {anime.episodes}
+            {anime?.episodes}
           </div>
           <div>
             <b>Source: </b>
-            {anime.source}
+            {anime?.source}
           </div>
           <div>
             <b>Status: </b>
-            {anime.status}
+            {anime?.status}
           </div>
           <div>
             <b>Type: </b>
-            {anime.type}
+            {anime?.type}
           </div>
           <div>
             <b>Aired: </b>
-            {anime.aired.string}
+            {anime?.aired?.string}
           </div>
           <div>
             <b>Duration: </b>
-            {anime.duration}
+            {anime?.duration}
           </div>
           <div>
             <b>Rank: </b>
-            {anime.rank}
+            {anime?.rank}
           </div>
           <div>
             <b>Season: </b>
-            {anime.season}
+            {anime?.season}
           </div>
           <div>
             <b>Producers: </b>
             <div className="inline-flex gap-2 flex-wrap">
-              {anime.producers?.map((item) => (
-                <a href={item.url} key={item.mal_id} className="underline">
-                  {item.name}
+              {anime?.producers?.map((item) => (
+                <a href={item?.url} key={item?.mal_id} className="underline">
+                  {item?.name}
                 </a>
               ))}
             </div>
--- components/jikan-anime/trailer-youtube.tsx
+++ components/jikan-anime/trailer-youtube.tsx
@@ -7,7 +7,7 @@ export const TrailerYoutube = ({ data }: { data: AnimeListData["trailer"] }) => 
   return (
     <div className="w-full bg-black rounded-lg overflow-hidden">
       <Youtube
-        videoId={data.youtube_id}
+        videoId={data?.youtube_id}
         onReady={(e) => e.target.pauseVideo()}
         opts={{ width: "100%", height: "240" }}
         // className="h-[490px]"
