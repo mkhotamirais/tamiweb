174fee826125cd981c23d331a827296bbd5ed203
Mkhotamirais <mkhotamirais@gmail.com>
2024-08-13 10:50:18 +0700

    projects

--- app/(root)/portfolio/_components/project.tsx
+++ app/(root)/portfolio/_components/project.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ProjectItem from "./project-item";
 import React from "react";
 import useSectionView from "./useSectionView";
 import { Projects } from "../../_components/projects";
@@ -10,12 +9,6 @@ export default function Project() {
 
   return (
     <section id="project" ref={ref} className="scroll-mt-16">
-      {/* <Title>My Projects</Title> */}
-      {/* <div className="flex flex-col gap-2">
-        {projectList.map((item, i) => (
-          <ProjectItem key={i} {...item} />
-        ))}
-      </div> */}
       <Projects className="" />
     </section>
   );
